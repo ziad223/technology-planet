af6887e2bf41c7b83361a5a4a6ea083796505113
ziad abdalla <ziadabdalla846@gmail.com>
2025-12-21 16:37:25 +0200

    connect categories and products and clients and articles

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import logo from '@/public/images/footer-logo.svg'
 export default function Loading() {
   return (
     <div className="flex h-screen w-full items-center justify-center">
        <Image
-              src={logo}
+              src='/images/logo.png'
               alt="Logo"
               width={320}
               height={320}
